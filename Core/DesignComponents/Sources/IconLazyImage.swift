edab63ade78394ed1b71d63a166c9a27efb312e9
Jun Xiu Chan <51901925+missingems@users.noreply.github.com>
2024-05-01 23:39:43 +0800

    feat: use designcomponents color

--- Core/DesignComponents/Sources/IconLazyImage.swift
+++ Core/DesignComponents/Sources/IconLazyImage.swift
@@ -5,7 +5,7 @@ public struct IconLazyImage: View {
   private let url: URL?
   private let tintColor: Color
   
-  public init(_ url: URL?, tintColor: Color = .accentColor) {
+  public init(_ url: URL?, tintColor: Color = DesignComponentsAsset.accentColor.swiftUIColor) {
     self.url = url
     self.tintColor = tintColor
   }
