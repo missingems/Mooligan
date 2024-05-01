0fd2aabcffa6315fa4fc21d2ec1f5ffb09d171f0
Jun Xiu Chan <51901925+missingems@users.noreply.github.com>
2024-05-01 12:23:42 +0800

    feat: setup tests for browserequestclient

--- Core/Networking/Sources/Requests/BrowseRequest.swift
+++ Core/Networking/Sources/Requests/BrowseRequest.swift
@@ -1,10 +1,22 @@
 import Foundation
+import OSLog
 import ScryfallKit
 
 public struct BrowseRequest: Request {
+  public enum ClientType {
+    case scryfall
+    case mock(BrowseRequestClient)
+  }
+  
   public let client: BrowseRequestClient
   
-  public init() {
-    client = ScryfallClient(networkLogLevel: .minimal)
+  public init(_ clientType: ClientType = .scryfall) {
+    switch clientType {
+    case .scryfall:
+      client = ScryfallClient(networkLogLevel: .minimal)
+      
+    case let .mock(client):
+      self.client = client
+    }
   }
 }
--- Core/Networking/Tests/NetworkingTests.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-import XCTest
-
-final class NetworkingTests: XCTestCase {
-  func test_twoPlusTwo_isFour() {
-    XCTAssertEqual(2+2, 4)
-  }
-}
-
