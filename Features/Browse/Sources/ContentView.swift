7ce02b5cbae2db57180ed4a56792642f20065f63
Jun Xiu Chan <51901925+missingems@users.noreply.github.com>
2024-05-01 11:20:04 +0800

    feat: setup BrowseRequest, client and example

--- Features/Browse/Sources/ContentView.swift
+++ Features/Browse/Sources/ContentView.swift
@@ -1,11 +1,24 @@
 import SwiftUI
+import Networking
 
 public struct ContentView: View {
-  public init() {}
+  @State
+  var sets: [Networking.Set] = []
+  
+  let request = BrowseRequest()
+  
+  public init() {
+    
+  }
   
   public var body: some View {
-    Text("Browse")
+    Text("\(sets.count)")
       .padding()
+      .onAppear {
+        Task {
+          sets = try await request.client.getAllSets()
+        }
+      }
   }
 }
 
