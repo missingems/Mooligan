fd8e4291240aa084bad165555e3bde1b6fb55491
Jun Xiu Chan <51901925+missingems@users.noreply.github.com>
2024-05-03 22:40:50 +0800

    feat: SetsView preview with live data

---
+++ Features/Browse/Sources/Sets/SetRowViewModel.swift
@@ -0,0 +1,39 @@
+import Foundation
+import SwiftUI
+
+extension SetRow {
+  struct ViewModel: Equatable {
+    let childIndicatorImageName: String
+    let disclosureIndicatorImageName: String
+    let iconUrl: URL?
+    let id: String
+    var colorScheme: ColorScheme
+    var isSelected: Bool
+    let shouldShowIndentIndicator: Bool
+    let numberOfCardsLabel: String
+    let shouldSetBackground: Bool
+    let title: String
+    
+    init(
+      iconURL: URL?,
+      id: String,
+      colorScheme: ColorScheme,
+      isHighlighted: Bool,
+      index: Int,
+      numberOfCards: Int,
+      shouldShowIndentIndicator: Bool,
+      title: String
+    ) {
+      childIndicatorImageName = "arrow.turn.down.right"
+      disclosureIndicatorImageName = "chevron.right"
+      iconUrl = iconURL
+      self.id = id.uppercased()
+      self.colorScheme = colorScheme
+      self.isSelected = isHighlighted
+      self.shouldShowIndentIndicator = shouldShowIndentIndicator
+      numberOfCardsLabel = String(localized: "\(numberOfCards) Cards")
+      shouldSetBackground = index.isMultiple(of: 2)
+      self.title = title
+    }
+  }
+}
