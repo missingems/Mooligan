d6b298f69a0d936a93e97bf48ec61060fa2b345d
Jun Xiu Chan <51901925+missingems@users.noreply.github.com>
2024-05-03 22:00:49 +0800

    feat: updated sinkablebuttonstyle

--- Core/DesignComponents/Sources/SinkableButtonStyle.swift
+++ Core/DesignComponents/Sources/SinkableButtonStyle.swift
@@ -3,9 +3,10 @@ import SwiftUI
 public struct SinkableButtonStyle: ButtonStyle {
   public func makeBody(configuration: Configuration) -> some View {
     configuration.label
-      .scaleEffect(configuration.isPressed ? 0.95 : 1)
-      .opacity(configuration.isPressed ? 0.382 : 1.0)
-      .animation(.interactiveSpring, value: configuration.isPressed)
+      .scaleEffect(configuration.isPressed ? CGSize(width: 0.98, height: 0.98) : CGSize(width: 1, height: 1))
+      .opacity(configuration.isPressed ? 0.3 : 1)
+      .contentShape(Rectangle())
+      .animation(.easeInOut(duration: configuration.isPressed ? 0.13 : 0.35), value: configuration.isPressed)
   }
 }
 
