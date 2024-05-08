7740638938e32126f90005f1bdba530f3d937e12
Jun Xiu Chan <51901925+missingems@users.noreply.github.com>
2024-05-08 15:03:09 +0800

    feat: Conform ScryfallClient to MagicCardRequestClient

---
+++ Core/Networking/Sources/Privates/ScryfallClient+MagicCardRequestClient.swift
@@ -0,0 +1,26 @@
+import ScryfallKit
+
+extension ScryfallClient: MagicCardQueryRequestClient {
+  public func queryCards(
+    _ query: MagicCardQueryType
+  ) async throws -> [any MagicCard] {
+    switch query {
+    case let .search(query, page):
+      fatalError("Search is not implemented yet")
+      
+    case let .set(gameSet, page):
+      let result = try await searchCards(
+        filters: [.set(gameSet.code)],
+        unique: .prints,
+        order: nil,
+        sortDirection: nil,
+        includeExtras: true,
+        includeMultilingual: false,
+        includeVariations: true,
+        page: page
+      )
+      
+      return []
+    }
+  }
+}
