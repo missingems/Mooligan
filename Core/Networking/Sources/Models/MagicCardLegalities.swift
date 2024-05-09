00caae34ae097d7056db74d93f7f0532855c7dcd
Jun Xiu Chan <51901925+missingems@users.noreply.github.com>
2024-05-09 21:21:16 +0800

    feat: created required protocols for MagicCard

--- Core/Networking/Sources/Models/MagicCardLegalities.swift
+++ Core/Networking/Sources/Models/MagicCardLegalities.swift
@@ -1,39 +1,17 @@
-public enum MagicCardLegalitiesValue: Codable, Hashable, Sendable {
-  public let standard: Legality?
-  public let historic: Legality?
-  public let pioneer: Legality?
-  public let modern: Legality?
-  public let legacy: Legality?
-  public let pauper: Legality?
-  public let vintage: Legality?
-  public let penny: Legality?
-  public let commander: Legality?
-  public let brawl: Legality?
-  
-  public init(standard: Legality?,
-              historic: Legality?,
-              pioneer: Legality?,
-              modern: Legality?,
-              legacy: Legality?,
-              pauper: Legality?,
-              vintage: Legality?,
-              penny: Legality?,
-              commander: Legality?,
-              brawl: Legality?) {
-    self.standard = standard
-    self.historic = historic
-    self.pioneer = pioneer
-    self.modern = modern
-    self.legacy = legacy
-    self.pauper = pauper
-    self.vintage = vintage
-    self.penny = penny
-    self.commander = commander
-    self.brawl = brawl
-  }
+public enum MagicCardLegalitiesValue: Equatable, Sendable {
+  case standard(MagicCardLegality)
+  case historic(MagicCardLegality)
+  case pioneer(MagicCardLegality)
+  case modern(MagicCardLegality)
+  case legacy(MagicCardLegality)
+  case pauper(MagicCardLegality)
+  case vintage(MagicCardLegality)
+  case penny(MagicCardLegality)
+  case commander(MagicCardLegality)
+  case brawl(MagicCardLegality)
 }
 
-public enum MagicCardLegality {
+public enum MagicCardLegality: Equatable, Sendable {
   /// This card is legal to be played in this format
   case legal
   /// This card is restricted in this format (players may only have one copy in their deck)
@@ -42,17 +20,8 @@ public enum MagicCardLegality {
   case banned
   /// This card is not legal in this format (ex: an uncommon is not legal in pauper)
   case notLegal
-  
-  public var label: String {
-    switch self {
-    case .notLegal:
-      return "Not Legal"
-    default:
-      return rawValue.capitalized
-    }
-  }
 }
 
-protocol MagicCardLegalities {
-  var value: [MagicCardLegalities] { get }
+public protocol MagicCardLegalities {
+  var value: [MagicCardLegalitiesValue] { get }
 }
