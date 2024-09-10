c3d5ada9e10d6651dc0c359dd43815166fbba5ec
Jun Xiu Chan <51901925+missingems@users.noreply.github.com>
2024-09-10 21:59:20 +0800

    Update MagicCardFixtures.swift

--- Core/Networking/Sources/CardDetail/Fixtures/MagicCardFixtures.swift
+++ Core/Networking/Sources/CardDetail/Fixtures/MagicCardFixtures.swift
@@ -6,16 +6,6 @@ public enum MagicCardFixtures {
   public var value: some MagicCard {
     MagicCardBuilder<MockMagicCardColor>()
       .with(imageURL: URL(string: "https://cards.scryfall.io/normal/front/0/2/023b5e6f-10de-422d-8431-11f1fdeca246.jpg?1562895407")!)
-      .with(name: "Eidolon")
-      .with(manaCost: "{R}{1}{0}")
-      .with(
-        printedText:
-"""
-At the beginning of each player's upkeep, Roiling Vortex deals 1 damage to them.
-Whenever a player casts a spell, if no mana was spent to cast that spell, Roiling Vortex deals 5 damage to that player.
-{R}: Your opponents can't gain life this turn.
-"""
-      )
       .with(set: "MKM")
       .with(collectorNumber: "123")
       .with(typeline: "Creature - Enchanment")
@@ -29,10 +19,14 @@ Whenever a player casts a spell, if no mana was spent to cast that spell, Roilin
           oracleText: "Oracle",
           power: "123",
           printedName: "Hahaha",
-          printedText: "Text",
+          printedText: """
+At the beginning of each player's upkeep, Roiling Vortex deals 1 damage to them.
+Whenever a player casts a spell, if no mana was spent to cast that spell, Roiling Vortex deals 5 damage to that player.
+{R}: Your opponents can't gain life this turn.
+""",
           printedTypeLine: "Topdecker123",
           toughness: "33",
-          typeLine: "Go away",
+          typeLine: "Topdecker123",
           manaCost: "{R}{1}{0}"
         ),
         .init(
@@ -47,8 +41,8 @@ Whenever a player casts a spell, if no mana was spent to cast that spell, Roilin
           printedText: "Text",
           printedTypeLine: "Topdecker123",
           toughness: "33",
-          typeLine: "Go away",
-          manaCost: "{R}{1}{0}"
+          typeLine: "Topdecker123",
+          manaCost: "{B}{1}{0}"
         ),
       ])
       .with(
