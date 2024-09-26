c1535145c5609f49e23cbc838650891f3547408c
Jun Xiu Chan <51901925+missingems@users.noreply.github.com>
2024-09-26 22:00:14 +0800

    Update CardDetailTableView.swift

--- Features/CardDetail/Sources/Widgets/CardDetailTableView.swift
+++ Features/CardDetail/Sources/Widgets/CardDetailTableView.swift
@@ -6,57 +6,54 @@ struct CardDetailTableView<Card: MagicCard>: View {
   let alternate: Content<Card>.Description?
   
   var body: some View {
-    if let alternate {
-      HStack(alignment: .top) {
-        TitleView(
-          name: main.name,
-          manaCost: main.manaCost
-        )
-        
-        Divider()
-        
-        TitleView(
-          name: alternate.name,
-          manaCost: alternate.manaCost
-        )
-      }
+    HStack(alignment: .top) {
+      TitleView(
+        name: main.name,
+        manaCost: main.manaCost
+      )
       
       Divider()
       
-      HStack(alignment: .top) {
-        TypelineView(main.typeline)
-        Divider()
-        TypelineView(alternate.typeline)
-      }
-      
+      TitleView(
+        name: alternate?.name,
+        manaCost: alternate?.manaCost
+      )
+    }
+    
+    Divider()
+    
+    HStack(alignment: .top) {
+      TypelineView(main.typeline)
       Divider()
-      
-      HStack(alignment: .top) {
-        DescriptionView(main.text)
-        Divider()
-        DescriptionView(alternate.text)
-      }
-      
+      TypelineView(alternate?.typeline)
+    }
+    
+    Divider()
+    
+    HStack(alignment: .top) {
+      DescriptionView(main.text)
       Divider()
-      
-      HStack(alignment: .top) {
-        FlavorView(main.flavorText)
-        Divider()
-        FlavorView(alternate.flavorText)
-      }
+      DescriptionView(alternate?.text)
+    }
+    
+    Divider()
+    
+    HStack(alignment: .top) {
+      FlavorView(main.flavorText)
+      Divider()
+      FlavorView(alternate?.flavorText)
     }
   }
   
   init?(descriptions: [Content<Card>.Description]) {
-    guard
-      descriptions.isEmpty == false,
-      descriptions.count <= 2,
-      let main = descriptions.first
-    else {
+    if descriptions.count == 1, let main = descriptions.first {
+      self.main = main
+      self.alternate = nil
+    } else if descriptions.count == 2, let main = descriptions.first, let alternate = descriptions.last {
+      self.main = main
+      self.alternate = alternate
+    } else {
       return nil
     }
-    
-    self.main = main
-    alternate = descriptions.last
   }
 }
