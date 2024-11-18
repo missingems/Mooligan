818ea09c333bfbf36ca3277029184707ac14c84b
Jun <51901925+missingems@users.noreply.github.com>
2024-11-18 15:38:35 +0800

    Update RulingView.swift

--- Features/CardDetail/Sources/Widgets/RulingView.swift
+++ Features/CardDetail/Sources/Widgets/RulingView.swift
@@ -13,13 +13,21 @@ struct RulingView: View {
       Text("Rulings").font(.headline)
         .safeAreaPadding(.horizontal, nil)
       
-      LazyVStack(alignment: .leading, spacing: 13.0) {
+      LazyVStack(alignment: .leading, spacing: 5) {
         ForEach(rulings.indices, id: \.self) { index in
           let ruling = rulings[index]
           
           VStack(alignment: .leading, spacing: 3) {
             Text(ruling.displayDate).font(.caption).foregroundStyle(.secondary)
-            Text(LocalizedStringKey(ruling.description)).font(.caption)
+            Text(LocalizedStringKey(ruling.description)).font(.caption).multilineTextAlignment(.leading)
+              .frame(maxWidth: .infinity, alignment: .leading)
+          }
+          .padding(.vertical, 8.0)
+          .safeAreaPadding(.horizontal, nil)
+          .background {
+            if index.isMultiple(of: 2) {
+              Color.primary.opacity(0.02).background(.ultraThinMaterial).clipShape(RoundedRectangle(cornerRadius: 13.0))
+            }
           }
           .safeAreaPadding(.horizontal, nil)
         }
