e9edd2094067826b5adcffbfa10cd141b3c30aea
Jun <51901925+missingems@users.noreply.github.com>
2024-11-30 16:17:19 +0800

    wip: Attempt to dynamically set color base on color scheme from color set

--- Core/DesignComponents/Sources/IconLazyImage.swift
+++ Core/DesignComponents/Sources/IconLazyImage.swift
@@ -1,15 +1,18 @@
 import Nuke
 import SVGView
 import SwiftUI
+import UIKit
 
 public struct IconLazyImage: View {
+  @Environment(\.colorScheme) private var colorScheme
   private let url: URL?
   @State private var imageData: Data?
   @State private var shouldAnimate: Bool = false
-  @Environment(\.colorScheme) private var colorScheme
+  private let tintColor: Color
   
-  public init(_ url: URL?) {
+  public init(_ url: URL?, tintColor: Color = DesignComponentsAsset.accentColor.swiftUIColor) {
     self.url = url
+    self.tintColor = tintColor
   }
   
   public var body: some View {
@@ -18,9 +21,10 @@ public struct IconLazyImage: View {
         let view = SVGView(data: imageData)
         
         if let svg = view.svg {
-          view
-            .task(id: colorScheme, priority: .background) {
-              tint(svg, colorScheme: colorScheme)
+          
+          view.task(id: colorScheme, priority: .background) {
+            print(UIColor.label.resolvedColor(with: .current))
+            tint(svg, tintColor: tintColor)
           }
         }
       }
@@ -51,24 +55,58 @@ public struct IconLazyImage: View {
     }
   }
   
-  private func tint(_ node: SVGNode, colorScheme: ColorScheme) {
-    let color: SVGColor
-    
-    switch colorScheme {
-    case .light:
-      color = SVGColor(hex: "0E1250")
-    case .dark:
-      color = SVGColor(hex: "F8F6D8")
-    @unknown default:
-      color = SVGColor(hex: "0E1250")
-    }
-    
+  private func tint(_ node: SVGNode, tintColor: Color) {
     if let group = node as? SVGGroup {
       for content in group.contents {
-        tint(content, colorScheme: colorScheme)
+        tint(content, tintColor: tintColor)
       }
-    } else if let shape = node as? SVGShape {
-      shape.fill = color
+    } else if
+      let shape = node as? SVGShape,
+      let colorHex = tintColor.toHex(colorScheme: colorScheme) {
+      shape.fill = SVGColor(hex: colorHex)
     }
+  }
+}
+
+private extension UIColor {
+  func toHex(includeAlpha: Bool = false, colorScheme: ColorScheme) -> String? {
+    let color = resolvedColor(with: UITraitCollection(userInterfaceStyle: colorScheme.toUIUserInterfaceStyle))
+    var red: CGFloat = 0
+    var green: CGFloat = 0
+    var blue: CGFloat = 0
+    var alpha: CGFloat = 0
+    let rgb = color.cgColor.components
+    print(rgb)
+    guard color.getRed(&red, green: &green, blue: &blue, alpha: &alpha) else {
+      return nil
+    }
+    
+    let r = Int(red * 255)
+    let g = Int(green * 255)
+    let b = Int(blue * 255)
+    let a = Int(alpha * 255)
+    
+    if includeAlpha {
+      return String(format: "%02X%02X%02X%02X", r, g, b, a)
+    } else {
+      return String(format: "%02X%02X%02X", r, g, b)
+    }
+  }
+}
+
+private extension ColorScheme {
+  var toUIUserInterfaceStyle: UIUserInterfaceStyle {
+    switch self {
+    case .light: return .light
+    case .dark: return .dark
+    default: return .light
+    }
+  }
+}
+
+private extension Color {
+  func toHex(includeAlpha: Bool = false, colorScheme: ColorScheme) -> String? {
+    let uiColor = UIColor(self)
+    return uiColor.toHex(includeAlpha: includeAlpha, colorScheme: colorScheme)
   }
 }
