24356949e81c8eef835a56d041a53733f5b066ac
Jun <51901925+missingems@users.noreply.github.com>
2024-12-27 01:01:15 +0800

    Update Feature.swift

--- Mooligan/Sources/Feature.swift
+++ Mooligan/Sources/Feature.swift
@@ -1,7 +1,45 @@
-//
-//  Feature.swift
-//  Mooligan
-//
-//  Created by Jun on 26/12/24.
-//
+import ComposableArchitecture
 
+@Reducer struct Feature<Client: GameSetRequestClient> {
+  let client: Client
+  
+  @ObservableState struct State: Equatable {
+  }
+  
+  enum Action: Equatable {
+    case didSelectSet(index: Int)
+    case fetchSets
+    case viewAppeared
+    case updateSets([Client.GameSetModel])
+  }
+  
+  var body: some ReducerOf<Self> {
+    Reduce { state, action in
+      switch action {
+      case let .didSelectSet(index):
+        return .none
+        
+      case .fetchSets:
+        state.isLoading = true
+        
+        return .run { send in
+          try await send(.updateSets(client.getAllSets()))
+        }
+        
+      case .viewAppeared:
+        return if state.sets.isEmpty {
+          .run { send in
+            await send(.fetchSets)
+          }
+        } else {
+          .none
+        }
+        
+      case let .updateSets(value):
+        state.isLoading = false
+        state.sets = IdentifiedArrayOf<Client.GameSetModel>(uniqueElements: value)
+        return .none
+      }
+    }
+  }
+}
