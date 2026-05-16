7965bf2cbaafe55898c0d07ed8c14d87a6bc2dde
Jun <51901925+missingems@users.noreply.github.com>
2026-05-16 13:52:35 +0800

    Add zoomAnimation to RootView
    Expose a Namespace.ID on RootView and forward it to SetsView. Update the initializer to accept a `zoomAnimation` namespace and store it on the view so child views can use matchedGeometryEffect-based zoom animations.

--- Features/Browse/Sources/RootView.swift
+++ Features/Browse/Sources/RootView.swift
@@ -4,12 +4,14 @@ import SwiftUI
 
 public struct RootView: View {
   public var store: StoreOf<BrowseFeature>
+  public var zoomAnimation: Namespace.ID
   
   public var body: some View {
-    SetsView(store: store)
+    SetsView(store: store, zoomAnimation: zoomAnimation)
   }
   
-  public init(store: StoreOf<BrowseFeature>) {
+  public init(store: StoreOf<BrowseFeature>, zoomAnimation: Namespace.ID) {
     self.store = store
+    self.zoomAnimation = zoomAnimation
   }
 }
