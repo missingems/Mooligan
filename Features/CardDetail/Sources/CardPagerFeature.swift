79619d7b26efcddfb4e6c276f7d0d403fcd1362e
Jun <51901925+missingems@users.noreply.github.com>
2026-05-25 23:07:47 +0800

    Update CardPagerFeature.swift

--- Features/CardDetail/Sources/CardPagerFeature.swift
+++ Features/CardDetail/Sources/CardPagerFeature.swift
@@ -9,15 +9,29 @@ public struct CardPagerFeature: Sendable {
   public struct State: Equatable {
     public var cards: IdentifiedArrayOf<CardDetailFeature.State>
     public var selectedId: UUID?
-    @Presents var showRulings: RulingFeature.State?
+    @Presents public var showRulings: RulingFeature.State?
     
-    public init(cards: [Card], initialSelectedCard: Card, queryType: QueryType) {
-      self.cards = IdentifiedArray(
-        uniqueElements: cards.map {
-          CardDetailFeature.State(card: $0, queryType: queryType)
-        }
-      )
+    // Store the raw data to be mapped lazily off the main thread
+    var rawCardDetails: [CardInfo]
+    var queryType: QueryType
+    
+    public init(cardDetails: [CardInfo], initialSelectedCard: Card, queryType: QueryType) {
+      self.rawCardDetails = cardDetails
+      self.queryType = queryType
       self.selectedId = initialSelectedCard.id
+      
+      // FAST PATH: Only initialize the tapped card right now.
+      if let initialInfo = cardDetails.first(where: { $0.card.id == initialSelectedCard.id }) {
+        self.cards = [
+          CardDetailFeature.State(
+            card: initialInfo.card,
+            displayableCardImage: initialInfo.displayableCardImage,
+            queryType: queryType
+          )
+        ]
+      } else {
+        self.cards = []
+      }
     }
   }
   
@@ -25,6 +39,10 @@ public struct CardPagerFeature: Sendable {
     case binding(BindingAction<State>)
     case cards(IdentifiedActionOf<CardDetailFeature>)
     case showRulings(PresentationAction<RulingFeature.Action>)
+    
+    // Lazy Loading actions
+    case task
+    case _setRemainingCards(IdentifiedArrayOf<CardDetailFeature.State>)
   }
   
   public var body: some ReducerOf<Self> {
@@ -32,6 +50,31 @@ public struct CardPagerFeature: Sendable {
     Reduce { state, action in
       switch action {
       case .binding:
+        return .none
+        
+      case .task:
+        // Prevent re-mapping if we've already loaded the full list
+        guard state.cards.count < state.rawCardDetails.count else { return .none }
+        
+        let rawDetails = state.rawCardDetails
+        let query = state.queryType
+        
+        return .run { send in
+          // Map the rest of the cards off the main thread
+          let mapped = rawDetails.map { info in
+            CardDetailFeature.State(
+              card: info.card,
+              displayableCardImage: info.displayableCardImage,
+              queryType: query
+            )
+          }
+          
+          await send(._setRemainingCards(IdentifiedArray(uniqueElements: mapped)))
+        }
+        
+      case let ._setRemainingCards(fullArray):
+        // Silently swap the array
+        state.cards = fullArray
         return .none
         
       case let .cards(.element(id: id, action: .viewRulingsTapped)):
